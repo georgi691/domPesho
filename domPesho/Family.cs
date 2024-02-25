8c769959702dcd5a0d6f1c96830778d599be5127
User <User@DESKTOP-BV3D47O>
2024-02-25 10:18:11 +0200

    Add project files.

---
+++ domPesho/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domPesho
+{
+    class Family
+    {
+        private List<Person> members = new List<Person>();
+
+        public void AddMember(Person person)
+        {
+            members.Add(person);
+        }
+
+        public void PrintFamily()
+        {
+            members.Sort();
+            foreach (var person in members)
+            {
+                Console.WriteLine($"{person.Name} {person.Age}");
+            }
+        }
+    }
+}
---
+++ domPesho/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Person : IComparable<Person>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public int CompareTo(Person other)
+    {
+        return string.Compare(Name, other.Name, StringComparison.Ordinal);
+    }
+}
